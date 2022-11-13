8e9c6fbe2d5873d7f96ab3ad939c62981bcbb0ff
schaffsp <schaffsp@dukes.jmu.edu>
2022-11-13 18:52:43 -0500

    Progess 2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import NavBar from './NavBar';
 import HeaderImage from './images/header_image.png'
-import JarImage from './images/jar_image.png'
+import Landfill from './images/landfill.jpeg'
 import Tractor from './images/farm-tractor-icon.svg';
 import Customize from './images/customize.svg';
 
@@ -14,36 +14,31 @@ function App() {
         </div>
         <div className="Header">
           <h1>We're not just protecting your packages. Every product we produce takes harmful CO2 out of the atmosphere.</h1>
-          <button>Learn More</button>
+          <button>LEARN MORE</button>
         </div>
       </div>
+      
+      {/* First two cards. */}
       <div className='Section-Container'>
         <div className="Section-Element">
-          <img src={Customize} width={100} alt="Tractor"/>
+          <img src={Customize} width={100} alt="Customize"/>
           <div>
             <h2 className="Section-Header">Customize your solution</h2>
             <p className="Section-Paragraph">Let us tailor your solution to best fit your business needs. Our packaging can be customized down to the details to ensure that Pure Shenandoah adds as much value as possible to your organization. Our team is ready to answer any questions about how our packaging can best integrate with your supply chain. Reach out to example@pureshenandoah.com to learn more!</p>
-        
           </div>  
         </div>
         <div className="Section-Element">
           <img src={Tractor} width={150} alt="Tractor"/>
           <div>
             <h2 className="Section-Header">Support farming communities</h2>
-            <p className="Section-Paragraph">From the outset, the goal of developing industrial hemp products was to get as many acres of local farmland planted as possible. Pure Shenandoah has always had a close relationship with our growers. Our company started alongside the 2018 farm bill, which hoped to reignite American agriculture, and remains committed to helping the communities that we owe so much of our success to.</p>
-        
+            <p className="Section-Paragraph">From the outset, the goal of developing industrial hemp products was to get as many acres of local farmland planted as possible. Pure Shenandoah has always had a close relationship with our growers. Our company started alongside the 2018 farm bill, which hoped to reignite American agriculture, and remains committed to supporting the communities that we owe so much of our success to.</p>
           </div>  
         </div>
       </div>
-      <div className='Section-Container'>
-        <div className="Section-Element" style={{ backgroundImage: `url(${JarImage})`, backgroundRepeat:"no-repeat", backgroundSize:"cover" }}>
-        </div>
-        <div className='Section-Element'>
-          <img src={Tractor} width={100} alt="Tractor"/>
-          <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h2>
-        </div>
+      {/* Quote. */}
+      <div className='Quote-Container' style={{ backgroundImage: `url(${Landfill})`, backgroundRepeat:"no-repeat", backgroundSize:"cover" }}>
+        <h1><span className='Quote-Main'>"Styrofoam takes 500 years to decompose; it cannot be recycled”</span><span className='Quote-Attribution'><br/>-Colgate University</span></h1>
       </div>
-      
     </div>
   );
 }
